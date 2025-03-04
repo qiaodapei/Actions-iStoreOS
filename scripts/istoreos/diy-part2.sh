57d267a491f435c09eb8a87e142f30c3dfb55210
qiaodapei <qiaodapei@hotmail.com>
2025-03-04 23:35:35 +0800

    Update diy-part2.sh

--- scripts/istoreos/diy-part2.sh
+++ scripts/istoreos/diy-part2.sh
@@ -303,15 +303,6 @@ popd
 
 # 添加第三方应用
 echo "
-# 启用pcre2开发库 
-CONFIG_PACKAGE_pcre2-host=y
-# 启用 LingTiGameAcc
-CONFIG_PACKAGE_LingTiGameAcc=y
-# 强制启用 netdata-ssl
-CONFIG_PACKAGE_netdata-ssl=y
-# 启用 NTFS3 内核模块
-CONFIG_PACKAGE_kmod-fs-ntfs3=y
-
 # 系统相关应用
 CONFIG_PACKAGE_luci-app-poweroff=y
 CONFIG_PACKAGE_luci-app-fileassistant=y
@@ -369,6 +360,14 @@ CONFIG_PACKAGE_xray-core=y
 CONFIG_PACKAGE_chinadns-ng=y
 CONFIG_PACKAGE_simple-obfs=y
 
+# 启用pcre2开发库 
+CONFIG_PACKAGE_pcre2-host=y
+# 启用 LingTiGameAcc
+CONFIG_PACKAGE_LingTiGameAcc=y
+# 强制启用 netdata-ssl
+CONFIG_PACKAGE_netdata-ssl=y
+# 启用 NTFS3 内核模块
+CONFIG_PACKAGE_kmod-fs-ntfs3=y
 
 #Passwall和Passwall2
 # CONFIG_PACKAGE_luci-app-passwall2=y
