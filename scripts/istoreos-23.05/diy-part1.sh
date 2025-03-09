2387c1ab845535df9800b5c6cb968993773fb535
qiaodapei <qiaodapei@hotmail.com>
2025-03-09 12:35:26 +0800

    Update diy-part1.sh

--- scripts/istoreos-23.05/diy-part1.sh
+++ scripts/istoreos-23.05/diy-part1.sh
@@ -24,3 +24,6 @@
 # # 添加 Passwall2 的软件源到 feeds 列表
 # echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main' >> feeds.conf.default
 # echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages.git' >> feeds.conf.default
+# 添加 Passwall2 的软件源到 feeds 列表
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git' >> feeds.conf.default
+echo 'src-git  passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default
